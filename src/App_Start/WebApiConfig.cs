e0c9426dadde27679f7d28b38a3250838e9d9dda
Pawel Cioch <pawel@greenhex.net>
2018-10-25 13:57:25 -0500

    added WebApiConfig example

---
+++ src/App_Start/WebApiConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using CompanyX.PaymentGateway.WebApi.Formatters;
+
+namespace CompanyX.PaymentGateway.WebApi
+{
+	public static class WebApiConfig
+	{
+		public static object HttpConfig { get; private set; }
+
+		public static void Register(HttpConfiguration config)
+		{
+			// Web API configuration and services
+
+			config.Formatters.Remove(config.Formatters.XmlFormatter);  //remove original
+
+			As400XmlMediaTypeFormatter formatter = new As400XmlMediaTypeFormatter();
+			formatter.UseXmlSerializer = true;
+
+			config.Formatters.Add(formatter); //add modified
+
+			// ...
+		}
+	}
+}
